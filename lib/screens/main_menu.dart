1b4734a338ed6acbd4de1b45b3b0a3f63f393b96
Nicky Valentino <nicky.valentino@sehatq.com>
2025-03-27 16:07:39 +0700

    grid dashboard

--- lib/screens/main_menu.dart
+++ lib/screens/main_menu.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
+
+import 'offers.dart';
+import 'hotels.dart';
+import 'flights.dart';
+import 'trains.dart';
+import 'todos.dart';
+import 'events.dart';
+import 'paylater.dart';
+import 'rewards.dart';
 
 class MainMenu extends StatefulWidget {
   const MainMenu({super.key});
@@ -17,37 +25,340 @@ class MainMenuState extends State<MainMenu> {
         title: Text(
           "Grid Dashboard",
           style: GoogleFonts.poppins(
-              fontSize: 24,
-              fontWeight: FontWeight.w600
+            fontSize: 24,
+            fontWeight: FontWeight.w600,
+            color: Colors.white,
           ),
         ),
-        backgroundColor: Color(0xFFD5F0C1),
+        backgroundColor: Color(0xFF4CC9FE),
       ),
       body: Container(
-        color: Color(0xFFF9F7C9),
-        child: Column(
+        color: Color(0xFFF7F9F2),
+        child: GridView.count(
+          crossAxisCount: 2,
+          crossAxisSpacing: 16,
+          padding: EdgeInsets.all(16),
           children: [
-            Center(
-                child: Container(
-                    margin: EdgeInsets.only(top: 24),
-                    width: MediaQuery.of(context).size.width * 0.90,
-                    height: 175,
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                        color: Color(0xFFAAD9BB),
-                        borderRadius: BorderRadius.circular(8)
-                    ),
-                    child: Text(
-                      "Main Activity",
-                      textAlign: TextAlign.center,
-                      style: GoogleFonts.poppins(
-                        fontSize: 48,
-                        color: Colors.white,
-                      ),
+            Container(
+              margin: EdgeInsets.only(top: 8),
+              child: SizedBox(
+                width: 80,
+                height: 80,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Offers()),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFF37AFE1),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8)
                     )
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Icon(
+                        Icons.discount,
+                        color: Colors.white,
+                        size: 72,
+                      ),
+                      Text(
+                        "Offers",
+                        textAlign: TextAlign.center,
+                        style: GoogleFonts.poppins(
+                          color: Colors.white,
+                          fontSize: 24,
+                        )
+                      )
+                    ]
+                  ),
+                ),
+              )
+            ),
+
+            Container(
+                margin: EdgeInsets.only(top: 8),
+                child: SizedBox(
+                  width: 80,
+                  height: 80,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => Hotels()),
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Color(0xFF37AFE1),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8)
+                        )
+                    ),
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Icon(
+                            Icons.hotel,
+                            color: Colors.white,
+                            size: 72,
+                          ),
+                          Text(
+                              "Hotels",
+                              textAlign: TextAlign.center,
+                              style: GoogleFonts.poppins(
+                                color: Colors.white,
+                                fontSize: 24,
+                              )
+                          )
+                        ]
+                    ),
+                  ),
                 )
             ),
-          ],
+
+            Container(
+                margin: EdgeInsets.only(top: 8),
+                child: SizedBox(
+                  width: 80,
+                  height: 80,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => Flights()),
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Color(0xFF37AFE1),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8)
+                        )
+                    ),
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Icon(
+                            Icons.flight,
+                            color: Colors.white,
+                            size: 72,
+                          ),
+                          Text(
+                              "Flights",
+                              textAlign: TextAlign.center,
+                              style: GoogleFonts.poppins(
+                                color: Colors.white,
+                                fontSize: 24,
+                              )
+                          )
+                        ]
+                    ),
+                  ),
+                )
+            ),
+
+            Container(
+                margin: EdgeInsets.only(top: 8),
+                child: SizedBox(
+                  width: 80,
+                  height: 80,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => Trains()),
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Color(0xFF37AFE1),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8)
+                        )
+                    ),
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Icon(
+                            Icons.train,
+                            color: Colors.white,
+                            size: 72,
+                          ),
+                          Text(
+                              "Trains",
+                              textAlign: TextAlign.center,
+                              style: GoogleFonts.poppins(
+                                color: Colors.white,
+                                fontSize: 24,
+                              )
+                          )
+                        ]
+                    ),
+                  ),
+                )
+            ),
+
+            Container(
+                margin: EdgeInsets.only(top: 8),
+                child: SizedBox(
+                  width: 80,
+                  height: 80,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => Todos()),
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Color(0xFF37AFE1),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8)
+                        )
+                    ),
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Icon(
+                            Icons.hiking,
+                            color: Colors.white,
+                            size: 72,
+                          ),
+                          Text(
+                              "To-Dos",
+                              textAlign: TextAlign.center,
+                              style: GoogleFonts.poppins(
+                                color: Colors.white,
+                                fontSize: 24,
+                              )
+                          )
+                        ]
+                    ),
+                  ),
+                )
+            ),
+
+            Container(
+                margin: EdgeInsets.only(top: 8),
+                child: SizedBox(
+                  width: 80,
+                  height: 80,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => Events()),
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Color(0xFF37AFE1),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8)
+                        )
+                    ),
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Icon(
+                            Icons.local_play,
+                            color: Colors.white,
+                            size: 72,
+                          ),
+                          Text(
+                              "Events",
+                              textAlign: TextAlign.center,
+                              style: GoogleFonts.poppins(
+                                color: Colors.white,
+                                fontSize: 24,
+                              )
+                          )
+                        ]
+                    ),
+                  ),
+                )
+            ),
+
+            Container(
+                margin: EdgeInsets.only(top: 8),
+                child: SizedBox(
+                  width: 80,
+                  height: 80,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => Paylater()),
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Color(0xFF37AFE1),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8)
+                        )
+                    ),
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Icon(
+                            Icons.payments,
+                            color: Colors.white,
+                            size: 72,
+                          ),
+                          Text(
+                              "PayLater",
+                              textAlign: TextAlign.center,
+                              style: GoogleFonts.poppins(
+                                color: Colors.white,
+                                fontSize: 24,
+                              )
+                          )
+                        ]
+                    ),
+                  ),
+                )
+            ),
+
+            Container(
+                margin: EdgeInsets.only(top: 8),
+                child: SizedBox(
+                  width: 80,
+                  height: 80,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => Rewards()),
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Color(0xFF37AFE1),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8)
+                        )
+                    ),
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Icon(
+                            Icons.emoji_events,
+                            color: Colors.white,
+                            size: 72,
+                          ),
+                          Text(
+                              "Rewards",
+                              textAlign: TextAlign.center,
+                              style: GoogleFonts.poppins(
+                                color: Colors.white,
+                                fontSize: 24,
+                              )
+                          )
+                        ]
+                    ),
+                  ),
+                )
+            ),
+          ]
         ),
       ),
     );
---
+++ lib/screens/paylater.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'main_menu.dart';
+
+class Paylater extends StatelessWidget {
+  const Paylater({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Grid Dashboard",
+          style: GoogleFonts.poppins(
+            fontSize: 24,
+            fontWeight: FontWeight.w600,
+            color: Colors.white,
+          ),
+        ),
+        backgroundColor: Color(0xFF4CC9FE),
+      ),
+      body: Container(
+        color: Color(0xFFF7F9F2),
+        child: Column(
+          children: [
+            Center(
+              child: Container(
+                margin: EdgeInsets.only(top: 24),
+                width: MediaQuery.of(context).size.width * 0.90,
+                height: 175,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                    color: Color(0xFF37AFE1),
+                    borderRadius: BorderRadius.circular(8)
+                ),
+                child: Text(
+                  "Paylater",
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.poppins(
+                    fontSize: 48,
+                    color: Colors.white,
+                  ),
+                )
+              )
+            ),
+
+            Container(
+              margin: EdgeInsets.only(top: 48),
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width * 0.90,
+                height: 64,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(
+                      context,
+                      MaterialPageRoute(builder: (context) => MainMenu()),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFF37AFE1),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8)
+                    )
+                  ),
+                  child: Text(
+                    "Back to Main Menu",
+                    style: GoogleFonts.poppins(
+                      fontSize: 24,
+                      color: Colors.white,
+                    )
+                  )
+
+                )
+              )
+            ),
+          ]
+        ),
+      ),
+    );
+  }
+}
